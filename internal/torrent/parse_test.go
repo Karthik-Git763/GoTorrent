8caeb58563b21455438464204236392b693e96af
Karthik-Git763 <karthikpilakat@gmail.com>
2026-06-02 23:44:13 +0530

    Added parse test

---
+++ internal/torrent/parse_test.go
@@ -0,0 +1,22 @@
+package torrent
+
+import (
+	"os"
+	"testing"
+)
+
+func TestParseRealTorrent(t *testing.T) {
+	data, err := os.ReadFile("testdata/debian.torrent")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var tf TorrentFile
+	if err := tf.Parse(data); err != nil {
+		t.Fatal(err)
+	}
+
+	if tf.Name == "" || tf.PieceLength == 0 || len(tf.PieceHashes) == 0 {
+		t.Fatalf("bad parse: %+v", tf)
+	}
+}
